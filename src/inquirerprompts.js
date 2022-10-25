619dc96e94647c0fac83bf57c1ffd999286ae225
Goat <dpajbk@gmail.com>
2022-10-24 20:15:38 -0400

    getManagerInfo promise is working

--- src/inquirerprompts.js
+++ src/inquirerprompts.js
@@ -1,35 +1,68 @@
 const inquirer = require('inquirer');
-
+const employee = require('../lib/employee');
+const manager = require('../lib/manager')
 
 function getManagerInfo() {
-    inquirer.prompt([
-        {
-            type : "input",
-            message: "Manager name?",
-            name: "name"
-        },
-        {
-            type: "input",
-            message: "Manager's employee ID?",
-            name: "id",
-        },
-        {
-            type: "input",
-            message: "Manager's email address?",
-            name: "email",
-        },
-        {
-            type: "input",
-            message: "Manager's office number?",
-            name: "officenumber"
-        }
-    ])
-    .then((response) => {
-        console.log(response)
-    })
+    return Promise.resolve(inquirer.prompt([
+            {
+                type: "input",
+                message: "Manager name?",
+                name: "name"
+            },
+            {
+                type: "input",
+                message: "Manager's employee ID?",
+                name: "id",
+            },
+            {
+                type: "input",
+                message: "Manager's email address?",
+                name: "email",
+            },
+            {
+                type: "input",
+                message: "Manager's office number?",
+                name: "officenumber"
+            }
+        ])
+        
+)
+    // .then((response) => {
+    //     // new manager(response.officenumber)
+    //     console.log(response)
+    // })
 }
 
-function getEmployeeInfo() {
-  
+function newEmployee() {
+    response = inquirer.prompt([
+        {
+            type: 'list',
+            name: 'newemployee',
+            message: 'Add additional employee?',
+            choices: [
+                'Engineer',
+                'Intern',
+                'Finish (No further employees will be added)'
+            ]
+        }
+    ])
+    if (response === 'Engineer') {
+        const response2 = inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'Employee name?'
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: "Employee's github user name?",
+            },
+
+        ])
+        console.log(response2)
+    }
 }
 getManagerInfo()
+    .then((resolved) => console.log(resolved))
+// newEmployee()
