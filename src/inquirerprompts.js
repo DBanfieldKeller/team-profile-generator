9eec4f153c8d2a7602636edb3e6baac47519a1da
Goat <dpajbk@gmail.com>
2022-10-24 22:27:20 -0400

    new employee function added

--- src/inquirerprompts.js
+++ src/inquirerprompts.js
@@ -3,38 +3,37 @@ const employee = require('../lib/employee');
 const manager = require('../lib/manager')
 
 function getManagerInfo() {
-    return Promise.resolve(inquirer.prompt([
-            {
-                type: "input",
-                message: "Manager name?",
-                name: "name"
-            },
-            {
-                type: "input",
-                message: "Manager's employee ID?",
-                name: "id",
-            },
-            {
-                type: "input",
-                message: "Manager's email address?",
-                name: "email",
-            },
-            {
-                type: "input",
-                message: "Manager's office number?",
-                name: "officenumber"
-            }
-        ])
-        
-)
-    // .then((response) => {
-    //     // new manager(response.officenumber)
-    //     console.log(response)
-    // })
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Manager name?",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "Manager's employee ID?",
+            name: "id",
+        },
+        {
+            type: "input",
+            message: "Manager's email address?",
+            name: "email",
+        },
+        {
+            type: "input",
+            message: "Manager's office number?",
+            name: "officenumber"
+        }
+    ])
+
+        .then((response) => {
+            // add class info
+            console.log(response)
+        })
 }
 
 function newEmployee() {
-    response = inquirer.prompt([
+    inquirer.prompt([
         {
             type: 'list',
             name: 'newemployee',
@@ -42,27 +41,61 @@ function newEmployee() {
             choices: [
                 'Engineer',
                 'Intern',
-                'Finish (No further employees will be added)'
+                'Finish (No further employees will be added and your html will be generated)'
             ]
         }
     ])
-    if (response === 'Engineer') {
-        const response2 = inquirer.prompt([
-            {
-                type: 'input',
-                name: 'name',
-                message: 'Employee name?'
-            },
-            {
-                type: 'input',
-                name: 'github',
-                message: "Employee's github user name?",
-            },
-
-        ])
-        console.log(response2)
-    }
+        .then((response) => {
+            if (response.newemployee === 'Engineer') {
+                inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'name',
+                        message: 'Employee name?'
+                    },
+                    {
+                        type: 'input',
+                        name: 'email',
+                        message: 'Employee email address?'
+                    },
+                    {
+                        type: 'input',
+                        name: 'github',
+                        message: "Employee's github user name?",
+                    },
+                ]).then((response) => {
+                    console.log(response);
+                    // add class stuff
+                    newEmployee()
+                })
+            } else if (response.newemployee === 'Intern') {
+                inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'name',
+                        message: 'Intern name?'
+                    },
+                    {
+                        type: 'input',
+                        name: 'email',
+                        message: 'Intern email address?',
+                    },
+                    {
+                        type: 'input',
+                        name: 'school',
+                        message: "Intern's school?"
+                    }
+                ]) .then((response) =>{
+                    console.log(response);
+                    // class stuff;
+                    newEmployee();
+                })
+            } else {
+                console.log('running html function')
+                // run html function
+            }
+        });
 }
-getManagerInfo()
-    .then((resolved) => console.log(resolved))
-// newEmployee()
+// getManagerInfo()
+    // .then((aooke) => console.log(aooke.name))
+newEmployee()
